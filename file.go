cec7001639ce945a07c2f8efb85a6e929af73a18
billy <billy@erigame.com>
2017-04-22 00:00:31 +0800

    add file utility function

--- file.go
+++ file.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gpmgo/gopm/modules/log"
 )
 
 //IsExist 判断一个文件或者文件夹是否存在
@@ -26,4 +30,36 @@ func WriteJSONToFile(file string, data interface{}) error {
 		return err
 	}
 	return nil
+}
+
+//GetFileListByExt 根据文件后缀名获取文件列表
+func GetFileListByExt(path string, ext string) ([]string, error) {
+	flist := make([]string, 0)
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		fname := f.Name()
+		if strings.HasSuffix(fname, ext) {
+			flist = append(flist, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return flist, nil
+}
+
+// GetCurrentPath 获取当前路径
+func GetCurrentPath() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	return wd, nil
 }
