886604dd4c2f46f3b74780c4ffb6861ff36430d8
billy <billy@erigame.com>
2017-06-29 21:01:23 +0800

    add test surport

---
+++ test/test.go
@@ -0,0 +1,28 @@
+package testing
+
+import (
+	"runtime/debug"
+	"testing"
+)
+
+// T wrap testing.T for addtion function
+type T struct {
+	*testing.T
+}
+
+// Assert check the condition
+// if false then error out message
+func (t *T) Assert(ok bool, format string, args ...interface{}) {
+	if !ok {
+		t.Errorf(format, args...)
+	}
+}
+
+// AssertNil check errors
+// if not nil then error out message and stack
+func (t *T) AssertNil(err error) {
+	if err != nil {
+		t.Error(t)
+		t.Log(string(debug.Stack()))
+	}
+}
